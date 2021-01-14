e2147abdae3123a9096016b9e9a4d763bedf7d6e
Fang MinJie <minjie_fang@odao.com>
2021-01-14 10:55:59 +0800

    Upgrade libcommon to 1.0.0.M3

--- buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
 	// spring boot
 	api(platform("org.springframework.boot:spring-boot-dependencies:2.4.1"))
 	// libcommon
-	api(platform("com.github.fmjsjx:libcommon-bom:1.0.0.M1"))
+	api(platform("com.github.fmjsjx:libcommon-bom:1.0.0.M3"))
 
     constraints {
         api("io.lettuce:lettuce-core:6.0.2.RELEASE")
