7417722f38d622ea24af63b95d6b7f48afecb19b
Fang MinJie <minjie_fang@odao.com>
2021-01-20 14:01:23 +0800

    Add projects myboot-starter-kafka & myboot-starter-pulsar

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,7 @@ rootProject.name = "myboot"
 include(":myboot-autoconfigure")
 include(":myboot-bom")
 include(":myboot-example")
+include(":myboot-starter-kafka")
 include(":myboot-starter-mongodb")
+include(":myboot-starter-pulsar")
 include(":myboot-starter-redis")
