f014411f26435eaab19dc00a5a2be7350b15a4ad
Fang MinJie <minjie_fang@odao.com>
2021-01-22 14:58:20 +0800

    try to fix myboot-example

--- myboot-example/build.gradle.kts
+++ myboot-example/build.gradle.kts
@@ -13,21 +13,21 @@ description = "MyBoot/Example"
 
 dependencies {
 
-	compileOnly("org.projectlombok:lombok")
-  	annotationProcessor("org.projectlombok:lombok")
-	implementation("org.slf4j:slf4j-api")
-	api(project(":myboot-starter-mongodb"))
-	api(project(":myboot-starter-redis"))
-	api("org.springframework.boot:spring-boot-starter") {
-	    exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
-	}
-	implementation("org.springframework.boot:spring-boot-starter-log4j2")
-	annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor")
-	compileOnly("org.springframework.boot:spring-boot-configuration-processor")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	
-	implementation(group = "io.netty", name = "netty-tcnative-boringssl-static", classifier = "linux-aarch_64")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("org.slf4j:slf4j-api")
+    api(project(":myboot-starter-mongodb"))
+    api(project(":myboot-starter-redis"))
+    implementation("org.springframework.boot:spring-boot-starter") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor")
+    compileOnly("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation(group = "io.netty", name = "netty-tcnative-boringssl-static", classifier = "linux-aarch_64")
     implementation(group = "io.netty", name = "netty-tcnative-boringssl-static", classifier = "linux-x86_64")
     implementation(group = "io.netty", name = "netty-tcnative-boringssl-static", classifier = "osx-x86_64")
     implementation(group = "io.netty", name = "netty-tcnative-boringssl-static", classifier = "windows-x86_64")
