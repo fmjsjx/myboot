d255d6885abda95ddd65d2559dddcb9e313396e9
Fang MinJie <minjie_fang@odao.com>
2021-01-22 14:55:48 +0800

    Fix myboot-example

--- myboot-example/build.gradle.kts
+++ myboot-example/build.gradle.kts
@@ -16,9 +16,9 @@ dependencies {
 	compileOnly("org.projectlombok:lombok")
   	annotationProcessor("org.projectlombok:lombok")
 	implementation("org.slf4j:slf4j-api")
-	implementation(project(":myboot-starter-mongodb"))
-	implementation(project(":myboot-starter-redis"))
-	implementation("org.springframework.boot:spring-boot-starter") {
+	api(project(":myboot-starter-mongodb"))
+	api(project(":myboot-starter-redis"))
+	api("org.springframework.boot:spring-boot-starter") {
 	    exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
 	}
 	implementation("org.springframework.boot:spring-boot-starter-log4j2")
