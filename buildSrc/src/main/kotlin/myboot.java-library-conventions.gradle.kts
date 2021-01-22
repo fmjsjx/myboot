550da0e8343f3a5bc7286eee568e53c592ef262e
Fang MinJie <minjie_fang@odao.com>
2021-01-22 17:51:08 +0800

    OO

--- buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
 	// spring boot
 	api(platform("org.springframework.boot:spring-boot-dependencies:2.4.1"))
+	annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:2.4.1"))
 	// libcommon
 	api(platform("com.github.fmjsjx:libcommon-bom:1.0.0.M6"))
 	// netty
