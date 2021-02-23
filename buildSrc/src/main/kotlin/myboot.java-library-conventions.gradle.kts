fdbd098bb45c5790c2fd0e9eec979d85e802ee19
fmjsjx <fmjsjx@163.com>
2021-02-23 18:26:20 +0800

    Upgrade libcommon to 1.0.2.Final for bug fixing

--- buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
@@ -8,12 +8,12 @@ repositories {
 
 dependencies {
     // spring boot
-    api(platform("org.springframework.boot:spring-boot-dependencies:2.4.2"))
-    annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:2.4.2"))
+    api(platform("org.springframework.boot:spring-boot-dependencies:2.4.3"))
+    annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:2.4.3"))
     // libcommon
-    api(platform("com.github.fmjsjx:libcommon-bom:1.0.1.Final"))
+    api(platform("com.github.fmjsjx:libcommon-bom:1.0.2.Final"))
     // netty
-    api(platform("io.netty:netty-bom:4.1.58.Final"))
+    api(platform("io.netty:netty-bom:4.1.59.Final"))
     // ALIYUN ONS
     api(platform("com.aliyun.openservices:ons-client:1.8.7.3.Final"))
     // rocketmq
