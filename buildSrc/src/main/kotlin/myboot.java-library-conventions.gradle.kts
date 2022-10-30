8ad068d5a17ece35eed22e54921e8cc91ce30b33
fmjsjx <fmjsjx@163.com>
2022-10-31 01:28:23 +0800

    Upgrade dependencies
    ```
    spring-boot     2.6.3         > 2.6.13
    libcommon       2.6.2         > 2.7.3
    libnetty        2.4.3         > 2.6.1
    netty           4.1.74.Final  > 4.1.84.Final
    ons-client      2.0.0.Final   > 2.0.4.Final
    rocketmq        4.9.2         > 4.9.4
    lettuce         6.1.6.RELEASE > 6.2.1.RELEASE
    kafka-clients   3.1.0         > 3.3.1
    pulsar-client   2.9.1         > 2.10.2
    mongodb         4.5.0         > 4.7.2
    log4j           2.17.1        > 2.19.0
    ```

--- buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
@@ -11,32 +11,33 @@ repositories {
 
 dependencies {
     // spring boot
-    api(platform("org.springframework.boot:spring-boot-dependencies:2.6.3"))
-    annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:2.6.3"))
+    api(platform("org.springframework.boot:spring-boot-dependencies:2.6.13"))
+    annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:2.6.13"))
     // libcommon
-    api(platform("com.github.fmjsjx:libcommon-bom:2.6.2"))
+    api(platform("com.github.fmjsjx:libcommon-bom:2.7.3"))
     // libnetty
-    api(platform("com.github.fmjsjx:libnetty-bom:2.4.3"))
+    api(platform("com.github.fmjsjx:libnetty-bom:2.6.1"))
     // netty
-    api(platform("io.netty:netty-bom:4.1.74.Final"))
+    api(platform("io.netty:netty-bom:4.1.84.Final"))
     // ALIYUN ONS
-    api(platform("com.aliyun.openservices:ons-client:2.0.0.Final"))
+    api(platform("com.aliyun.openservices:ons-client:2.0.4.Final"))
     // rocketmq
-    api(platform("org.apache.rocketmq:rocketmq-all:4.9.2"))
+    api(platform("org.apache.rocketmq:rocketmq-all:4.9.4"))
 
     constraints {
-        api("io.lettuce:lettuce-core:6.1.6.RELEASE")
-        api("org.apache.kafka:kafka-clients:3.1.0")
-        api("org.apache.pulsar:pulsar-client:2.9.1")
+        api("io.lettuce:lettuce-core:6.2.1.RELEASE")
+        api("org.apache.kafka:kafka-clients:3.3.1")
+        api("org.apache.pulsar:pulsar-client:2.10.2")
         // MongoDB
-        api("org.mongodb:bson:4.5.0")
-        api("org.mongodb:mongodb-driver-core:4.5.0")
-        api("org.mongodb:mongodb-driver-sync:4.5.0")
-        api("org.mongodb:mongodb-driver-reactivestreams:4.5.0")
-        api("org.mongodb:mongodb-driver-legacy:4.5.0")
+        val mongodbVersion = "4.7.2"
+        api("org.mongodb:bson:$mongodbVersion")
+        api("org.mongodb:mongodb-driver-core:$mongodbVersion")
+        api("org.mongodb:mongodb-driver-sync:$mongodbVersion")
+        api("org.mongodb:mongodb-driver-reactivestreams:$mongodbVersion")
+        api("org.mongodb:mongodb-driver-legacy:$mongodbVersion")
     }
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.17.1"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.19.0"))
 
 }
 
