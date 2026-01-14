34cb8fc2e24b33c9c53279f6aed88bbb2afbf9ef
fmjsjx <fmjsjx@163.com>
2026-01-14 11:48:04 +0800

    Upgrade libnetty to 4.1.0-RC

--- buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     // libcommon
     implementation(platform("com.github.fmjsjx:libcommon-bom:4.1.0-RC"))
     // libnetty
-    api(platform("com.github.fmjsjx:libnetty-bom:4.0.0"))
+    api(platform("com.github.fmjsjx:libnetty-bom:4.1.0-RC"))
 
 }
 
