51b15396e374f8f5ef04601ecf160807c8203747
fmjsjx <fmjsjx@163.com>
2026-01-14 11:18:19 +0800

    Upgrade libcommon to 4.1.0-RC

--- buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     api(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     // libcommon
-    implementation(platform("com.github.fmjsjx:libcommon-bom:4.0.0"))
+    implementation(platform("com.github.fmjsjx:libcommon-bom:4.1.0-RC"))
     // libnetty
     api(platform("com.github.fmjsjx:libnetty-bom:4.0.0"))
 
