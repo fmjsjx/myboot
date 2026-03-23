6eadc5dda412c4ef968db0f84b094eea2b9b88bb
fmjsjx <fmjsjx@163.com>
2026-03-23 23:21:15 +0800

    [Chore] Bump to LibCommon 4.1.5

--- buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     api(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     // libcommon
-    implementation(platform("com.github.fmjsjx:libcommon-bom:4.1.3"))
+    implementation(platform("com.github.fmjsjx:libcommon-bom:4.1.5"))
     // libnetty
     api(platform("com.github.fmjsjx:libnetty-bom:4.1.2"))
 
