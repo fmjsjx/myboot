f1e69f9e0d76c4ad225b6418028a875ad7f057f5
fmjsjx <fmjsjx@163.com>
2026-03-30 17:25:54 +0800

    [Chore] Bump Spring Boot to 4.0.5

--- buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/myboot.java-library-conventions.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     // Spring Boot
-    val springBootVersion = "4.0.4"
+    val springBootVersion = "4.0.5"
     api(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     // libcommon
